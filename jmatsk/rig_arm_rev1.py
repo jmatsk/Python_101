252e086e65bc771c280597d032fa12c0459fb470
jmatsk <73870300+jmatsk@users.noreply.github.com>
2021-01-03 14:14:52 -0800

    Update rig_arm_rev1.py
    - renamed bind joints previously missed with 'bn_' prefix
    - reworked hardcoded lock&hide of FKIK switch attrs with loop
    - reworked hardcoded SDKs for FKIK switch with loops and if statements

--- jmatsk/rig_arm_rev1.py
+++ jmatsk/rig_arm_rev1.py
@@ -12,8 +12,8 @@ jntList = [[['bn_shoulder_jnt', [0, 0, 0]],['bn_elbow_jnt', [7, 0, -0.3]],['bn_w
 # loop to create joints; checks for finished chain and clears selection before new chain
 printCount = 0
 for chain in jntList:
-    for joint in chain:
-        cmds.joint(n=joint[0], p=joint[1])
+    for each in chain:
+        cmds.joint(n=each[0], p=each[1])
         printCount += 1
 
         if printCount == 3:
@@ -53,7 +53,6 @@ cmds.orientConstraint('wrist_ik_ctrl', 'ik_wrist_jnt')
 
 ### IK pole vector ###
 cmds.select(cl=True)
-
 # create locator for elbow PV
 cmds.spaceLocator(n='loc_elbow_pv')
 
@@ -63,9 +62,6 @@ posElbow = cmds.xform('ik_elbow_jnt', q=True, t=True, ws=True)
 # move pv loc to elbow joint pos
 cmds.xform('loc_elbow_pv', t=posElbow)
 
-# posLocElbTZ = cmds.getAttr('loc_elbow_pv.translateZ')
-# pvMove = posLocElbTZ - 2
-# print pvMove
 cmds.select('loc_elbow_pv')
 cmds.move(-10.2, z=1, relative=True)
 cmds.select(cl=True)
@@ -120,16 +116,14 @@ cmds.parent('elbow_fk_ctrl_grp', 'shoulder_fk_ctrl')
 cmds.parent('wrist_fk_ctrl_grp', 'elbow_fk_ctrl')
 
 cmds.select(cl=True)
-
 ### constrain rig joints to fk and ik chains
-cmds.parentConstraint('fk_shoulder_jnt', 'ik_shoulder_jnt', 'shoulder_jnt')
-cmds.parentConstraint('fk_elbow_jnt', 'ik_elbow_jnt', 'elbow_jnt')
-cmds.parentConstraint('fk_wrist_jnt', 'ik_wrist_jnt', 'wrist_jnt')
+cmds.parentConstraint('fk_shoulder_jnt', 'ik_shoulder_jnt', 'bn_shoulder_jnt')
+cmds.parentConstraint('fk_elbow_jnt', 'ik_elbow_jnt', 'bn_elbow_jnt')
+cmds.parentConstraint('fk_wrist_jnt', 'ik_wrist_jnt', 'bn_wrist_jnt')
 
 ### FK/IK switch ###
 # create circle for switch control that follows bind skeleton wrist and is always visible
 cmds.select(cl=True)
-# create nurbs circle for fkik switch
 cmds.circle(n='fkik_switch_ctrl', r=1, nr=[1, 0, 0])
 
 # create switch control grouper
@@ -151,19 +145,17 @@ cmds.makeIdentity(apply=True)
 cmds.select(cl=True)
 
 # constrain switch ctrl group to bind skeleton wris
-cmds.parentConstraint('wrist_jnt', 'fkik_switch_ctrl_grp')
+cmds.parentConstraint('bn_wrist_jnt', 'fkik_switch_ctrl_grp')
 
 # lock and hide switch ctrl attrs
-cmds.setAttr('fkik_switch_ctrl.tx', lock=True, keyable=False)
-cmds.setAttr('fkik_switch_ctrl.ty', lock=True, keyable=False)
-cmds.setAttr('fkik_switch_ctrl.tz', lock=True, keyable=False)
-cmds.setAttr('fkik_switch_ctrl.rx', lock=True, keyable=False)
-cmds.setAttr('fkik_switch_ctrl.ry', lock=True, keyable=False)
-cmds.setAttr('fkik_switch_ctrl.rz', lock=True, keyable=False)
-cmds.setAttr('fkik_switch_ctrl.sx', lock=True, keyable=False)
-cmds.setAttr('fkik_switch_ctrl.sy', lock=True, keyable=False)
-cmds.setAttr('fkik_switch_ctrl.sz', lock=True, keyable=False)
-cmds.setAttr('fkik_switch_ctrl.visibility', lock=True, keyable=False)
+attrCount = 0
+fkikAttrList = ['tx', 'ty', 'tz', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz', 'visibility']
+for each in fkikAttrList:
+    fkikAttr = 'fkik_switch_ctrl.' + fkikAttrList[attrCount]
+    cmds.setAttr(fkikAttr, lock=True, keyable=False)
+    print fkikAttr
+    attrCount += 1
+print 'Loop end'
 
 # add a float attr to fkik switch for keyable switch blend
 cmds.addAttr('fkik_switch_ctrl', longName='FK_IK_Switch', defaultValue=0.00, minValue=0.00, maxValue=10.00,
@@ -171,48 +163,60 @@ cmds.addAttr('fkik_switch_ctrl', longName='FK_IK_Switch', defaultValue=0.00, min
 # cmds.deleteAttr('fkik_switch_ctrl.FK_IK')
 
 # create SDKs for FKIK switch; set Attrs then create SDK ###
-# shoulder sdk
-cmds.setAttr("fkik_switch_ctrl.FK_IK_Switch", 0)
-cmds.setAttr("shoulder_jnt_parentConstraint1.fk_shoulder_jntW0", 1)
-cmds.setAttr("shoulder_jnt_parentConstraint1.ik_shoulder_jntW1", 0)
-cmds.setDrivenKeyframe('shoulder_jnt_parentConstraint1.fk_shoulder_jntW0',
-                       currentDriver='fkik_switch_ctrl.FK_IK_Switch')
-cmds.setDrivenKeyframe('shoulder_jnt_parentConstraint1.ik_shoulder_jntW1',
-                       currentDriver='fkik_switch_ctrl.FK_IK_Switch')
+fkikSwN = 'fkik_switch_ctrl.FK_IK_Switch'
 
-cmds.setAttr("fkik_switch_ctrl.FK_IK_Switch", 10)
-cmds.setAttr("shoulder_jnt_parentConstraint1.fk_shoulder_jntW0", 0)
-cmds.setAttr("shoulder_jnt_parentConstraint1.ik_shoulder_jntW1", 1)
-cmds.setDrivenKeyframe('shoulder_jnt_parentConstraint1.fk_shoulder_jntW0',
-                       currentDriver='fkik_switch_ctrl.FK_IK_Switch')
-cmds.setDrivenKeyframe('shoulder_jnt_parentConstraint1.ik_shoulder_jntW1',
-                       currentDriver='fkik_switch_ctrl.FK_IK_Switch')
+fkikAttrList = ['bn_shoulder_jnt_parentConstraint1.fk_shoulder_jntW0',
+                'bn_shoulder_jnt_parentConstraint1.ik_shoulder_jntW1',
+                'bn_elbow_jnt_parentConstraint1.fk_elbow_jntW0',
+                'bn_elbow_jnt_parentConstraint1.ik_elbow_jntW1',
+                'bn_wrist_jnt_parentConstraint1.fk_wrist_jntW0',
+                'bn_wrist_jnt_parentConstraint1.ik_wrist_jntW1', ]
 
-# elbow sdk
-cmds.setAttr("fkik_switch_ctrl.FK_IK_Switch", 0)
-cmds.setAttr("elbow_jnt_parentConstraint1.fk_elbow_jntW0", 1)
-cmds.setAttr("elbow_jnt_parentConstraint1.ik_elbow_jntW1", 0)
-cmds.setDrivenKeyframe('elbow_jnt_parentConstraint1.fk_elbow_jntW0', currentDriver='fkik_switch_ctrl.FK_IK_Switch')
-cmds.setDrivenKeyframe('elbow_jnt_parentConstraint1.ik_elbow_jntW1', currentDriver='fkik_switch_ctrl.FK_IK_Switch')
+fkikSwVal = [0, 1, 10]
 
-cmds.setAttr("fkik_switch_ctrl.FK_IK_Switch", 10)
-cmds.setAttr("elbow_jnt_parentConstraint1.fk_elbow_jntW0", 0)
-cmds.setAttr("elbow_jnt_parentConstraint1.ik_elbow_jntW1", 1)
-cmds.setDrivenKeyframe('elbow_jnt_parentConstraint1.fk_elbow_jntW0', currentDriver='fkik_switch_ctrl.FK_IK_Switch')
-cmds.setDrivenKeyframe('elbow_jnt_parentConstraint1.ik_elbow_jntW1', currentDriver='fkik_switch_ctrl.FK_IK_Switch')
+cmds.setAttr(fkikSwN, fkikSwVal[0])
+fkOn = cmds.getAttr(fkikSwN)
+print fkOn
 
-# wrist sdk
-cmds.setAttr("fkik_switch_ctrl.FK_IK_Switch", 0)
-cmds.setAttr("wrist_jnt_parentConstraint1.fk_wrist_jntW0", 1)
-cmds.setAttr("wrist_jnt_parentConstraint1.ik_wrist_jntW1", 0)
-cmds.setDrivenKeyframe('wrist_jnt_parentConstraint1.fk_wrist_jntW0', currentDriver='fkik_switch_ctrl.FK_IK_Switch')
-cmds.setDrivenKeyframe('wrist_jnt_parentConstraint1.ik_wrist_jntW1', currentDriver='fkik_switch_ctrl.FK_IK_Switch')
+printCount = 0
+for each in fkikAttrList:
+    # print each
+    cmds.setAttr(each, fkikSwVal[0])
+    # if FKIK switch is 0 (FK), set FK constraints to 1 (on)
+    if fkOn == 0:
+        cmds.setAttr(fkikAttrList[0], 1)
+        cmds.setAttr(fkikAttrList[2], 1)
+        cmds.setAttr(fkikAttrList[4], 1)
 
-cmds.setAttr("fkik_switch_ctrl.FK_IK_Switch", 10)
-cmds.setAttr("wrist_jnt_parentConstraint1.fk_wrist_jntW0", 0)
-cmds.setAttr("wrist_jnt_parentConstraint1.ik_wrist_jntW1", 1)
-cmds.setDrivenKeyframe('wrist_jnt_parentConstraint1.fk_wrist_jntW0', currentDriver='fkik_switch_ctrl.FK_IK_Switch')
-cmds.setDrivenKeyframe('wrist_jnt_parentConstraint1.ik_wrist_jntW1', currentDriver='fkik_switch_ctrl.FK_IK_Switch')
+for each in fkikAttrList:
+    cmds.setDrivenKeyframe(each, currentDriver=fkikSwN)
+    printCount += 1
+    print printCount
 
-# set switch to 0, or fk, by default
-cmds.setAttr("fkik_switch_ctrl.FK_IK_Switch", 0)
+    if printCount == 6:
+        print 'FK SDK done, set IK SDK'
+        printCount = 0
+        cmds.setAttr(fkikSwN, fkikSwVal[2])
+        fkOn = cmds.getAttr(fkikSwN)
+        print fkOn
+
+        for each in fkikAttrList:
+            cmds.setAttr(each, fkikSwVal[0])
+            # if FKIK switch is 10 (IK), set IK constraints to 1 (on)
+            if fkOn == 10:
+                cmds.setAttr(fkikAttrList[1], 1)
+                cmds.setAttr(fkikAttrList[3], 1)
+                cmds.setAttr(fkikAttrList[5], 1)
+
+for each in fkikAttrList:
+    cmds.setDrivenKeyframe(each, currentDriver=fkikSwN)
+    printCount += 1
+    print printCount
+
+    if printCount == 6:
+        print 'IK SDK done'
+        printCount = 0
+        # set switch to 0, or fk, by default
+        cmds.setAttr(fkikSwN, fkikSwVal[0])
+        fkOn = cmds.getAttr(fkikSwN)
+        print fkOn
